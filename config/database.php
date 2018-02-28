a315db9b44ebf2d3bc6d8f80f740963ab3a224e2
Danil Davydov <Linadakill@gmail.com>
2018-02-28 15:09:41 +0200

    For docker

--- config/database.php
+++ config/database.php
@@ -1,9 +1,4 @@
 <?php
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
 return [
     /*
     |--------------------------------------------------------------------------
@@ -13,7 +8,7 @@ return [
     | Here you may specify which of the database connections below you wish
     | to use as your default connection for all database work. Of course
     | you may use many connections at once using the Database library.
-    |
+    | //
     */
     'default' => env('DB_CONNECTION', 'mysql'),
     /*
@@ -31,23 +26,26 @@ return [
     | choice installed on your machine before you begin development.
     |
     */
-    
     'connections' => [
         'sqlite' => [
             'driver' => 'sqlite',
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
         ],
-        'mysql' => array(
-                'driver'    => 'mysql',
-                'host'      => $host,
-                'database'  => $database,
-                'username'  => $username,
-                'password'  => $password,
-                'charset'   => 'utf8',
-                'collation' => 'utf8_unicode_ci',
-                'prefix'    => '',
-            ),
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
